37fef85cce79a23592d88520d19d77c8b8286d17
23002su <23002@supnum.mr>
2025-03-09 20:38:45 +0100

    donegit add .git add .

--- timetable_app/users/models.py
+++ timetable_app/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    CATEGORY_CHOICES = [
+        ('admin', 'Admin'),
+        ('enseignant', 'Enseignant'),
+        ('etudiant', 'Étudiant'),
+    ]
+    
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='etudiant')
+    is_password_temp = models.BooleanField(default=True)
+    def __str__(self):
+        return self.username
