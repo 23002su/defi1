99c469bed13ce2c01420e07ffbceb43ed4a59e8b
khalive <23090@supnum.mr>
2025-03-10 10:31:37 -1200

    Delete defi1-khalive/timetable_app directory

--- defi1-khalive/timetable_app/timetable_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Django admin interface
-    path('', include('scheduler.urls')),  # Include scheduler app URLs
-]
